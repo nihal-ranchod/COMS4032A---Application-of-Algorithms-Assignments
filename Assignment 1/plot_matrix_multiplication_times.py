539006bf4aea1064d420feabccd682d1c2047e0e
nihal-ranchod <nihal.ranchod@gmail.com>
2024-08-04 19:35:49 +0200

    Add: code to plot runtimes and saved png

---
+++ Assignment 1/plot_matrix_multiplication_times.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Read the CSV file
+data = pd.read_csv("Assignment 1/matrix_multiplication_times_run2.csv")
+
+# Extract columns
+dimensions = data['Dimension']
+standard_times = data['Standard']
+recursive_times = data['Recursive']
+strassen_times = data['Strassen']
+
+# Plot the data
+plt.figure(figsize=(10, 6))
+plt.plot(dimensions, standard_times, label="Square Matrix Multiplication", marker='o', color='blue')
+plt.plot(dimensions, recursive_times, label="Square Matrix Multiplication Recursive", marker='o', color='red')
+plt.plot(dimensions, strassen_times, label="Strassen's Algorithm", marker='o', color='green')
+
+# Customize the plot
+plt.title('Running Times of Matrix Multiplication Algorithms')
+plt.xlabel('Dimension')
+plt.xticks([64, 96, 128, 160, 192, 224, 256, 288, 320, 352, 384, 416, 448, 480, 512])
+plt.ylabel('Time (seconds)')
+plt.legend()
+plt.grid(True)
+plt.savefig("Assignment 1/matrix_multiplication_runtimes.png")
+plt.show()
